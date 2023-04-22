13c3341433305bcc952aec21c20d5f95492edd05
akash-akya <akashh246@gmail.com>
2023-04-22 14:02:37 +0530

    Remove advdef to fix segmentation fault on github workflow

--- build/lin.sh
+++ build/lin.sh
@@ -534,9 +534,6 @@ tar chzf ${PACKAGE}/libvips-${VERSION_VIPS}-${PLATFORM}.tar.gz \
   *.json \
   THIRD-PARTY-NOTICES.md
 
-# Recompress using AdvanceCOMP, ~5% smaller
-advdef --recompress --shrink-insane ${PACKAGE}/libvips-${VERSION_VIPS}-${PLATFORM}.tar.gz
-
 # Recompress using Brotli, ~15% smaller
 gunzip -c ${PACKAGE}/libvips-${VERSION_VIPS}-${PLATFORM}.tar.gz | brotli -o ${PACKAGE}/libvips-${VERSION_VIPS}-${PLATFORM}.tar.br
 
