a7b010cb1f822e3754bfc10e64063f3865282c20
akash-akya <akashh246@gmail.com>
2023-04-22 13:59:53 +0530

    Fix repo links

--- build/lin.sh
+++ build/lin.sh
@@ -550,7 +550,7 @@ printf "{\n\
 printf "\"${PLATFORM}\"" >platform.json
 
 # Add third-party notices
-$CURL -O https://raw.githubusercontent.com/lovell/sharp-libvips/main/THIRD-PARTY-NOTICES.md
+$CURL -O https://raw.githubusercontent.com/akash-akya/sharp-libvips/main/THIRD-PARTY-NOTICES.md
 
 # Create the tarball
 ls -al lib
