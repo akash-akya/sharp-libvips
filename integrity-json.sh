ab8525d2f885d026b6b9d74409f900b2b96e95a6
akash-akya <akashh246@gmail.com>
2023-04-22 13:59:53 +0530

    Fix repo links

--- integrity-json.sh
+++ integrity-json.sh
@@ -3,7 +3,7 @@ set -e
 
 LIBVIPS_VERSION=$(cat LIBVIPS_VERSION)
 
-for url in $(curl -sL https://api.github.com/repos/lovell/sharp-libvips/releases | jq -r --arg NAME "v$LIBVIPS_VERSION" '.[] | select(.name == $NAME) | .assets[] | select(.name | contains(".br.integrity")) | .browser_download_url'); do
+for url in $(curl -sL https://api.github.com/repos/akash-akya/sharp-libvips/releases | jq -r --arg NAME "v$LIBVIPS_VERSION" '.[] | select(.name == $NAME) | .assets[] | select(.name | contains(".br.integrity")) | .browser_download_url'); do
   PLATFORM_AND_ARCH=$(echo $url | cut -d'.' -f6 | cut -d'-' -f2,3)
   CHECKSUM=$(curl -Ls $url)
   echo "\"$PLATFORM_AND_ARCH\": \"$CHECKSUM\","
