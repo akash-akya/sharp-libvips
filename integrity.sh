6ff613a6c3331b2ec23ec9018aebe35432695cc2
akash-akya <akashh246@gmail.com>
2025-07-13 11:53:35 +0530

    Add the old code to upload the release pkg directly

---
+++ integrity.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+## Copyright 2025 Akash Akya and others.
+## SPDX-License-Identifier: Apache-2.0
+
+# Calculate sha512-based checksum
+integrity() {
+  CHECKSUM="sha512-$(shasum -a 512 $1 | cut -f1 -d' ' | xxd -r -p | base64 | tr -d '\n')"
+  printf "$CHECKSUM $1\n"
+  printf "$CHECKSUM" > $1.integrity
+}
+
+for tarball in *.tar.gz; do
+  integrity $tarball
+done
